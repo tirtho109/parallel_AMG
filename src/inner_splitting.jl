fd55e8bd322da32195e78bc4e94f09bd6a113161
Tirtho Sarathi Saha <t.saha@tu-braunschweig.de>
2023-09-15 15:13:42 +0200

    clean up & Ref added

--- src/inner_splitting.jl
+++ src/inner_splitting.jl
@@ -1,3 +1,12 @@
+"""
+    RS_CF_splitting_inner(S::SparseMatrixCSC, T::SparseMatrixCSC)
+	Ref: 
+	1.	Michael Griebel, Bram Metsch, Daniel Oeltz, and Marc Alexander Schweitzer.
+		Coarse grid classification: a parallel coarsening scheme for algebraic multigrid meth-
+		ods. Numerical linear algebra with applications, 13(2-3):193–214, 2006.
+	2.	https://github.com/JuliaLinearAlgebra/AlgebraicMultigrid.jl (Pass 1)
+
+"""
 function RS_CF_splitting_inner(S::SparseMatrixCSC, T::SparseMatrixCSC)
 
 	n = size(S,1)
@@ -20,31 +29,9 @@ function RS_CF_splitting_inner(S::SparseMatrixCSC, T::SparseMatrixCSC)
         interval_count[lambda[i] + 1] += 1	# count how many node has the same lamda value
 		#@show lambda[i], (lambda[i]+1), interval_count'
     end
-	"""
-	SP = 10x10
-	(lambda[i], lambda[i] + 1, interval_count') = (1, 2, [0 1 0 0 0 0 0 0 0 0 0])
-	(lambda[i], lambda[i] + 1, interval_count') = (2, 3, [0 1 1 0 0 0 0 0 0 0 0])
-	(lambda[i], lambda[i] + 1, interval_count') = (2, 3, [0 1 2 0 0 0 0 0 0 0 0])
-	(lambda[i], lambda[i] + 1, interval_count') = (2, 3, [0 1 3 0 0 0 0 0 0 0 0])
-	(lambda[i], lambda[i] + 1, interval_count') = (2, 3, [0 1 4 0 0 0 0 0 0 0 0])
-	(lambda[i], lambda[i] + 1, interval_count') = (2, 3, [0 1 5 0 0 0 0 0 0 0 0])
-	(lambda[i], lambda[i] + 1, interval_count') = (2, 3, [0 1 6 0 0 0 0 0 0 0 0])
-	(lambda[i], lambda[i] + 1, interval_count') = (2, 3, [0 1 7 0 0 0 0 0 0 0 0])
-	(lambda[i], lambda[i] + 1, interval_count') = (2, 3, [0 1 8 0 0 0 0 0 0 0 0])
-	(lambda[i], lambda[i] + 1, interval_count') = (1, 2, [0 2 8 0 0 0 0 0 0 0 0])
-	"""
-	
+
 	# initial interval_ptr
 	@views accumulate!(+, interval_ptr[2:end], interval_count[1:end-1])
-	"""
-	julia> interval_ptr'
-	1×11 adjoint(::Vector{Int64}) with eltype Int64:
-	0  0  2  10  10  10  10  10  10  10  10
-	In this example, the value 2 at interval_ptr[3] means that 
-	the interval with lambda[i] being 1 ends at index 2 in the interval_count array. 
-	Similarly, the value 10 at interval_ptr[4] means that
-	the interval with lambda[i] being 2 ends at index 10 in the interval_count array.
-	"""
 
 	# sort the nodes by the number of nodes strongly coupled to them:
 	#   `index_to_node[idx]` is the node with `idx`th least number of nodes coupled to it
@@ -53,11 +40,7 @@ function RS_CF_splitting_inner(S::SparseMatrixCSC, T::SparseMatrixCSC)
 	#   sortperm!(index_to_node, lambda)
 	#   node_to_index[index_to_node] .= 1:n
 	interval_count .= 0 # temporarily zeroed, goes back to its original at end of loop
-	"""
-	julia> interval_count'
-	1×11 adjoint(::Vector{Int64}) with eltype Int64:
-	0  0  0  0  0  0  0  0  0  0  0
-	"""
+	
     for i = 1:n
         lambda_i = lambda[i] + 1
         interval_count[lambda_i] += 1
@@ -67,69 +50,7 @@ function RS_CF_splitting_inner(S::SparseMatrixCSC, T::SparseMatrixCSC)
 		#@show i,lambda_i, interval_count[lambda_i], index
 		#@show index_to_node[index], node_to_index[i]
     end
-	"""
-	julia> lambda'
-	1×10 adjoint(::Vector{Int64}) with eltype Int64:
-	1  2  2  2  2  2  2  2  2  1
-	julia> lambda_i
-	1×10 adjoint(::Vector{Int64}) with eltype Int64:
-	2  3  3  3  3  3  3  3  3  2
-	julia> interval_ptr'
-	1×11 adjoint(::Vector{Int64}) with eltype Int64:
-	0  0  2  10  10  10  10  10  10  10  10
-	julia> interval_count'
-	1×11 adjoint(::Vector{Int64}) with eltype Int64:
-	0  2  8  0  0  0  0  0  0  0  0 <--------- incremented in each step
 	
-	(i, interval_count') = (1, [0 1 0 0 0 0 0 0 0 0 0])
-	(i, interval_count') = (2, [0 1 1 0 0 0 0 0 0 0 0])
-	(i, interval_count') = (3, [0 1 2 0 0 0 0 0 0 0 0])
-	(i, interval_count') = (4, [0 1 3 0 0 0 0 0 0 0 0])
-	(i, interval_count') = (5, [0 1 4 0 0 0 0 0 0 0 0])
-	(i, interval_count') = (6, [0 1 5 0 0 0 0 0 0 0 0])
-	(i, interval_count') = (7, [0 1 6 0 0 0 0 0 0 0 0])
-	(i, interval_count') = (8, [0 1 7 0 0 0 0 0 0 0 0])
-	(i, interval_count') = (9, [0 1 8 0 0 0 0 0 0 0 0])
-	(i, interval_count') = (10, [0 2 8 0 0 0 0 0 0 0 0])
-
-	(i, lambda_i, interval_count[lambda_i], index) = (1, 2, 1, 1)
-	(index_to_node[index], node_to_index[i]) = (1, 1)
-
-	(i, lambda_i, interval_count[lambda_i], index) = (2, 3, 1, 3)
-	(index_to_node[index], node_to_index[i]) = (2, 3)
-
-	(i, lambda_i, interval_count[lambda_i], index) = (3, 3, 2, 4)
-	(index_to_node[index], node_to_index[i]) = (3, 4)
-
-	(i, lambda_i, interval_count[lambda_i], index) = (4, 3, 3, 5)
-	(index_to_node[index], node_to_index[i]) = (4, 5)
-
-	(i, lambda_i, interval_count[lambda_i], index) = (5, 3, 4, 6)
-	(index_to_node[index], node_to_index[i]) = (5, 6)
-
-	(i, lambda_i, interval_count[lambda_i], index) = (6, 3, 5, 7)
-	(index_to_node[index], node_to_index[i]) = (6, 7)
-
-	(i, lambda_i, interval_count[lambda_i], index) = (7, 3, 6, 8)
-	(index_to_node[index], node_to_index[i]) = (7, 8)
-
-	(i, lambda_i, interval_count[lambda_i], index) = (8, 3, 7, 9)
-	(index_to_node[index], node_to_index[i]) = (8, 9)
-
-	(i, lambda_i, interval_count[lambda_i], index) = (9, 3, 8, 10)
-	(index_to_node[index], node_to_index[i]) = (9, 10)
-
-	(i, lambda_i, interval_count[lambda_i], index) = (10, 2, 2, 2)
-	(index_to_node[index], node_to_index[i]) = (10, 2)
-
-	julia> index_to_node'
-	1×10 adjoint(::Vector{Int64}) with eltype Int64:
-	1  10  2  3  4  5  6  7  8  9
-
-	julia> node_to_index'
-	1×10 adjoint(::Vector{Int64}) with eltype Int64:
-	1  3  4  5  6  7  8  9  10  2
-	"""
 	splitting = fill(U_NODE, n)
 
     # all nodes which no other nodes are strongly coupled to become F nodes
@@ -154,18 +75,10 @@ function RS_CF_splitting_inner(S::SparseMatrixCSC, T::SparseMatrixCSC)
 	#	is the new highest lambda[i], the line: `interval_ptr[lambda_k+1] = new_pos - 1`
 	#	pushes the all the inactive C and F points to the end of the next now-non-empty 
 	#	interval.
-	"""
-	julia> index_to_node'
-	1×10 adjoint(::Vector{Int64}) with eltype Int64:
-	1  10  2  3  4  5  6  7  8  9
 
-	julia> node_to_index'
-	1×10 adjoint(::Vector{Int64}) with eltype Int64:
-	1  3  4  5  6  7  8  9  10  2
-	"""
 	for top_index = n:-1:1
 		i = index_to_node[top_index]
-		lambda_i = lambda[i] + 1		#lambda_i = 2  3  3  3  3  3  3  3  3  2
+		lambda_i = lambda[i] + 1		
 		interval_count[lambda_i] -= 1
 
 		splitting[i] == F_NODE && continue
@@ -253,7 +166,7 @@ function RS_CF_splitting_inner(S::SparseMatrixCSC, T::SparseMatrixCSC)
 		for j in nzrange(T,i)
 			push!(siT, T.rowval[j])
 		end
-		# @show js = intersect(si, intersect(siT, f_set))
+		
 		js = si ∩ siT ∩ f_set
 		#@show isempty(js)
 		if isempty(js)
@@ -264,13 +177,12 @@ function RS_CF_splitting_inner(S::SparseMatrixCSC, T::SparseMatrixCSC)
 				for ptr in nzrange(S,j)
 					push!(sj, S.rowval[ptr])
 				end
-				#intersection = intersect(si, intersect(sj, c_set))
+				
 				intersection = si ∩ sj ∩ c_set
-				#@show intersection
+				
 				if isempty(intersection)
 					splitting[j] = C_NODE
 					push!(new_c_set, j)
-					#@show splitting
 				end
 			end
 		end
